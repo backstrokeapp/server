80570c3bfe5c3acc97d7bad7d884bac5ed36b8f9
1egoman <rsg1egoman@gmail.com>
2016-06-28 16:21:43 -0400

    switch var -> let and other es2015 stuff

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,8 @@ import $ from 'jquery';
 
 // enable the continue button when a user and repo have been entered
 $("#user, #repo").on('keyup', function() {
-  var user = $("#user").val();
-  var repo = $("#repo").val();
+  let user = $("#user").val();
+  let repo = $("#repo").val();
   if (user.length && repo.length) {
     $("#next").prop('disabled', false);
   } else {
@@ -13,11 +13,12 @@ $("#user, #repo").on('keyup', function() {
 
 // show the instructions panel
 $("#next").on('click', function() {
-  var user = $("#user").val();
-  var repo = $("#repo").val();
+  let user = $("#user").val();
+  let repo = $("#repo").val();
   $(".user").html(user);
   $(".repo").html(repo);
 
   $(".new-webhook").attr("href", "https://github.com/"+user+"/"+repo+"/settings/hooks/new");
-  $(".rest").animate({opacity: 1}, 200);
+  $(".rest").css("display", "block");
+  $(".rest").animate({opacity: 1, height: 320}, 200);
 });

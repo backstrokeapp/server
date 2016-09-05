c6c91af057f1f94c38b957b405274ffc8e4c8ecf
Ryan Gaus <rsg1egoman@gmail.com>
2016-09-05 15:52:27 -0400

    code: style the user not authed page

--- public/js/components/UserNotAuthenticated.js
+++ public/js/components/UserNotAuthenticated.js
@@ -3,7 +3,9 @@ import {connect} from 'react-redux';
 
 export function UserNotAuthenticated({user}) {
   return <div className="user-not-authenticated">
-    <a href="/setup/login" className="btn btn-primary">Please login first</a>
+    <i className="fa fa-frown-o" />
+    <h1>Hmm, you don't seem to be logged in.</h1>
+    <a href="/setup/login" className="btn btn-primary">Sign in with Github</a>
   </div>;
 }
 

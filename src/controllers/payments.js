d47b18a4aa6e2b0753d6e4b74228791ea220b9cc
Ryan Gaus <rsg1egoman@gmail.com>
2016-09-15 07:16:47 -0400

    code(payments): write a stripe link layer

--- src/controllers/payments.js
+++ src/controllers/payments.js
@@ -2,39 +2,14 @@ import stripeFactory from 'stripe';
 const PRICE_PER_HOOK = 0.02;
 const stripe = stripeFactory(process.env.STRIPE_TOKEN);
 
-export function createCustomer(user, token) {
-  let customer;
-  return stripe.customers.create({
-    source: token,
-    description: `${user.provider}:${user.user}`, // the user's github username
-  }).then(cust => {
-    customer = cust;
-    return stripe.subscriptions.create({
-      customer: customer.id,
-      plan: 'private_repo',
-    });
-  }).then(subscription => {
-    return {
-      subscription,
-      customer,
-    };
-  });
-}
-
-export function addPrivateRepo(user) {
-  return stripe.subscriptions.retreive(user.subscriptionId).then(subsc => {
-    return stripe.subscriptions.update(user.subscriptionId, {quantity: ++subsc.quantity});
-  });
-}
-
-
+// Add card info to a user.
 export function addPaymentInfo(User, req, res) {
   if (req.isAuthenticated()) {
     return createCustomer(req.user, req.body.stripeToken).then(data => {
       return User.update({_id: req.user._id}, {
         customerId: data.customer.id,
         subscriptionId: data.subscription.id
-      });
+      }).exec();
     }).then(user => {
       res.status(200).send({status: 'ok'});
     }).catch(err => {
@@ -42,5 +17,28 @@ export function addPaymentInfo(User, req, res) {
     })
   } else {
     res.status(403).send({error: 'Not authenticated.'});
+  }
+}
+
+// Called when a paid link is added or changed
+export function addPaidLink(user, delta=1) {
+  if (user.customerId) {
+    // create or update subscription?
+    return stripe.subscriptions.retreive(user.subscriptionId).then(subsc => {
+      if (subsc) {
+        // update the subscription
+        subsc.quantity += delta;
+        return stripe.subscriptions.update(user.subscriptionId, {quantity: subs.quantity});
+      } else {
+        // create a subscription
+        return stripe.subscriptions.create({
+          customer: user.customerId,
+          plan: 'private_repo',
+          quantity: delta, // 0 + delta
+        });
+      }
+    });
+  } else {
+    return Promise.reject(new Error('Peyment info not specified.'));
   }
 }
--- src/models/User.js
+++ src/models/User.js
@@ -3,15 +3,15 @@ import mongoose from 'mongoose';
 const user = new mongoose.Schema({
   accessToken: 'string',
   refreshToken: 'string',
+
+  user: 'string',
+  email: 'string',
   provider: 'string',
   picture: 'string',
   provider_id: 'string',
-  user: 'string',
-  email: 'string',
+
   customerId: 'string', // from stripe
   subscriptionId: 'string', // also from stripe
 });
-
-// user.static.
 
 export default mongoose.model('User', user);

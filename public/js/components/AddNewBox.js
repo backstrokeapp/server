0bf782663eef424edd57343b87bd079ecb565c82
Ryan Gaus <rsg1egoman@gmail.com>
2016-10-07 18:09:20 -0400

    code: highlight the first logical step and disable the 2nd step until the first is complete

--- public/js/components/AddNewBox.js
+++ public/js/components/AddNewBox.js
@@ -1,13 +1,20 @@
+/* vim: set syntax=javascript.jsx : */
 import React from 'react';
 import {connect} from 'react-redux';
+import classname from 'classname'
 
 import addAllForks from 'actions/addAllForks';
 import addNewRepository from 'actions/addNewRepository';
 
 // Add a new repository. A repo is the only thing that can go in a from slot.
-export function AddNewBox({type, onAddNewRepository, onAddAllForks}) {
+export function AddNewBox({type, onAddNewRepository, onAddAllForks, dim}) {
   return <div className="add-new-box">
-    <div className="add-new-option">
+    <div className={classname(
+      "add-new-option",
+      `add-new-option-${type}`, {
+        'add-new-option-muted': dim
+      })
+    }>
       <button className="btn" onClick={onAddNewRepository.bind(null, type)}>
         <i className="fa fa-plus-square-o" />
         {
@@ -19,8 +26,13 @@ export function AddNewBox({type, onAddNewRepository, onAddAllForks}) {
     </div>
     {
       type === 'to' ?
-      <div className="add-new-option">
-        <button className="btn" onClick={onAddAllForks.bind(null, type)}>
+      <div className={classname(
+        "add-new-option",
+        "add-new-option-to", {
+          'add-new-option-muted': dim
+        })
+      }>
+        <button className="btn" onClick={dim ? null : onAddAllForks.bind(null, type)}>
           <i className="fa fa-plus-square-o" />
           Sync changes to all forks
         </button>

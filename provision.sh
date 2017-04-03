590f1d11767a423f8839b5a6909f33a946c586c0
Ryan Gaus <rsg1egoman@gmail.com>
2017-04-03 07:25:47 -0400

    build: add sudo

--- provision.sh
+++ provision.sh
@@ -16,7 +16,7 @@ fi
 if ! which docker-machine > /dev/null; then
   curl -L https://github.com/docker/machine/releases/download/v0.10.0/docker-machine-`uname -s`-`uname -m` >/tmp/docker-machine
   chmod +x /tmp/docker-machine
-  cp /tmp/docker-machine /usr/local/bin/docker-machine
+  sudo cp /tmp/docker-machine /usr/local/bin/docker-machine
 fi
 
 echo "* Creating droplet to deploy containers..."

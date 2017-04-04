5d152b485cba35a2cce2689b7caddc23a5c4bf34
Ryan Gaus <rsg1egoman@gmail.com>
2017-04-04 06:33:17 -0400

    code: handle the case where the volume is already detached.

--- provision.sh
+++ provision.sh
@@ -42,8 +42,10 @@ sleep 10
 
 echo "* Mounting shared volume for database in new droplet..."
 DROPLET_ONE="$(doctl compute droplet list $DROPLET_ONE_NAME --format ID | tail -1)"
-DB_VOLUME="$(doctl compute volume list backstroke-data --format ID | tail -1)"
-doctl compute volume-action detach $DB_VOLUME
+DB_VOLUME="$(doctl compute volume list backstroke-data --format Name | tail -1)"
+if ! doctl compute volume-action detach $DB_VOLUME; then
+  echo "* Looks like the volume was detached already."
+fi
 doctl compute volume-action attach $DB_VOLUME $DROPLET_ONE
 docker-machine ssh $DROPLET_ONE_NAME -- \
   "mkdir -p /data && mount -o discard,defaults /dev/disk/by-id/scsi-0DO_Volume_backstroke-data /data"

c777ed189a1f291ddfe8a36df6bcfbef3a5a4385
Ryan Gaus <rsg1egoman@gmail.com>
2017-07-07 07:23:22 -0400

    code(test): use async / await for webhook tests
    Much easier to read. ALso added more tests.

--- server/src/routes/webhook/test.js
+++ server/src/routes/webhook/test.js
@@ -13,41 +13,33 @@ const User = new MockModel(),
 Link.methods.display = function() { return this; }
 
 describe('webhook', () => {
-
-  it('should create a pull request when given a single fork', function() {
-    let userData, upstreamData, forkData;
-
-    const searchIssues = sinon.stub().resolves({total_issues: 0, issues: []});
+  it('should create a pull request when given a single fork', async () => {
+    const searchIssues = sinon.stub().resolves({total_count: 0, issues: []});
     const pullRequestsCreate = sinon.stub().resolves('pull request response');
 
-    return Promise.all([
-      User.create({username: 'ryan'}),
-      Repository.create({type: 'repo', owner: 'foo', repo: 'bar', branch: 'master'}), // Upstream
-      Repository.create({type: 'repo', owner: 'hello', repo: 'world', branch: 'master'}), // Fork
-    ]).then(([user, upstream, fork]) => {
-      userData = user;
-      upstreamData = upstream;
-      forkData = fork;
-      return Link.create({
-        name: 'My Link',
-        enabled: true,
-        hookId: ['123456'],
-        owner: user.id,
-        upstream: upstream.id,
-        fork: fork.id,
-      });
-    }).then(link => {
-      return webhook({
-        github: {
-          user: {
-            searchIssues,
-          },
-          bot: {
-            pullRequestsCreate,
-          },
-        }
-      }, link);
-    }).then(resp => {
+    const user = await User.create({username: 'ryan'});
+    const upstream = await Repository.create({type: 'repo', owner: 'foo', repo: 'bar', branch: 'master'});
+    const fork = await Repository.create({type: 'repo', owner: 'hello', repo: 'world', branch: 'master'});
+
+    const link = await Link.create({
+      name: 'My Link',
+      enabled: true,
+      hookId: ['123456'],
+      owner: user.id,
+      upstream: upstream.id,
+      fork: fork.id,
+    });
+
+    return webhook({
+      github: {
+        user: {
+          searchIssues,
+        },
+        bot: {
+          pullRequestsCreate,
+        },
+      }
+    }, link).then(resp => {
       assert.deepEqual(resp, {
         status: 'ok',
         pullRequest: 'pull request response',
@@ -69,44 +61,43 @@ describe('webhook', () => {
       ]);
     });
   });
-  it('should create a pull request on each fork when given a bunch of forks', function() {
-    let userData, upstreamData, forkData;
-
+  it('should create a pull request on each fork when given a bunch of forks', async () => {
     const reposGetForks = sinon.stub().withArgs({owner: 'foo', repo: 'bar'}).resolves([
       {owner: {login: 'hello'}, name: 'world'},
       {owner: {login: 'another'}, name: 'repo'},
     ]);
-    const searchIssues = sinon.stub().resolves({total_issues: 0, issues: []});
+    const searchIssues = sinon.stub().resolves({total_count: 0, issues: []});
     const pullRequestsCreate = sinon.stub().resolves('pull request response');
 
-    return Promise.all([
-      User.create({username: 'ryan'}),
-      Repository.create({type: 'repo', owner: 'foo', repo: 'bar', branch: 'master'}), // Upstream
-      Repository.create({type: 'fork-all'}), // Fork
-    ]).then(([user, upstream, fork]) => {
-      userData = user;
-      upstreamData = upstream;
-      return Link.create({
-        name: 'My Link',
-        enabled: true,
-        hookId: ['123456'],
-        owner: user.id,
-        upstream: upstream.id,
-        fork: fork.id,
-      });
-    }).then(link => {
-      return webhook({
-        github: {
-          user: {
-            searchIssues,
-            reposGetForks,
-          },
-          bot: {
-            pullRequestsCreate,
-          },
-        }
-      }, link);
-    }).then(resp => {
+    const user = await User.create({username: 'ryan'});
+    const upstream = await Repository.create({ 
+      type: 'repo',
+      owner: 'foo',
+      repo: 'bar',
+      branch: 'master'
+    });
+    const fork = await Repository.create({type: 'fork-all'}); // Fork
+     
+    const link = await Link.create({
+      name: 'My Link',
+      enabled: true,
+      hookId: ['123456'],
+      owner: user.id,
+      upstream: upstream.id,
+      fork: fork.id,
+    });
+
+    return webhook({
+      github: {
+        user: {
+          searchIssues,
+          reposGetForks,
+        },
+        bot: {
+          pullRequestsCreate,
+        },
+      }
+    }, link).then(resp => {
       assert.deepEqual(resp, {
         status: 'ok',
         forkCount: 2,
@@ -138,200 +129,98 @@ describe('webhook', () => {
       ]);
     });
   });
+  it('should try to make a PR to a single fork of an upstream, but the repo opted out', async () => {
+    const searchIssues = sinon.stub().resolves({total_count: 1, issues: [
+      {title: 'opted out!', labels: [{name: 'optout'}]},
+    ]});
+    const pullRequestsCreate = sinon.stub().resolves('pull request response');
 
-  // it('should try to make a PR to a single fork of an upstream, but the repo opted out', () => {
-  //   const upstream = generateRepo({
-  //     owner: 'upstream', name: 'my-app',
-  //     default_branch: 'master',
-  //     branches: ['master'],
-  //     issues: [],
-  //   });
-  //   const fork = generateRepo({
-  //     owner: 'fork', name: 'my-app',
-  //     default_branch: 'master',
-  //     branches: ['master'],
-  //     issues: [
-  //       {name: "My PR", labels: [{name: 'optout', id: generateId()}]},
-  //     ],
-  //   });
-  //   const gh = createMockGithubInstance([upstream, fork]);
-  //
-  //   const upstreamId = generateId(),
-  //         forkId = generateId();
-  //   const link = {
-  //     enabled: true,
-  //     upstreamId,
-  //     upstream: () => Promise.resolve({
-  //       id: upstreamId,
-  //       type: 'repo',
-  //       owner: upstream.owner.login,
-  //       repo: upstream.name,
-  //       branch: 'master',
-  //     }),
-  //     forkId,
-  //     fork: () => Promise.resolve({
-  //       id: forkId,
-  //       type: 'repo',
-  //       owner: fork.owner.login,
-  //       repo: fork.name,
-  //       branch: 'master',
-  //     }),
-  //   };
-  //
-  //   return webhook(gh, link, undefined, gh).then(output => {
-  //     assert.deepEqual(output, {
-  //       status: 'ok',
-  //       pullRequest: {msg: 'This repo opted out of backstroke pull requests'},
-  //       isEnabled: true,
-  //       many: false,
-  //       forkCount: 1,
-  //     });
-  //   });
-  // });
-  // it('should try to make a PR to a single fork of an upstream, but a pull request already exists', () => {
-  //   const upstream = generateRepo({
-  //     owner: 'upstream', name: 'my-app',
-  //     default_branch: 'master',
-  //     branches: ['master'],
-  //     issues: [],
-  //   });
-  //   const fork = generateRepo({
-  //     owner: 'fork', name: 'my-app',
-  //     default_branch: 'master',
-  //     branches: ['master'],
-  //     pullRequests: [
-  //       {
-  //         id: generateId(),
-  //         number: 42,
-  //         state: 'open',
-  //         title: 'My PR', 
-  //         body: '(This pull request already was made by backstroke previously.)',
-  //         labels: [],
-  //         head: {
-  //           label: 'upstream:master',
-  //         },
-  //         base: {
-  //           label: 'master',
-  //         }
-  //       },
-  //     ],
-  //   });
-  //   const gh = createMockGithubInstance([upstream, fork]);
-  //
-  //   const upstreamId = generateId(),
-  //         forkId = generateId();
-  //   const link = {
-  //     enabled: true,
-  //     upstreamId,
-  //     upstream: () => Promise.resolve({
-  //       id: upstreamId,
-  //       type: 'repo',
-  //       owner: upstream.owner.login,
-  //       repo: upstream.name,
-  //       branch: 'master',
-  //     }),
-  //     forkId,
-  //     fork: () => Promise.resolve({
-  //       id: forkId,
-  //       type: 'repo',
-  //       owner: fork.owner.login,
-  //       repo: fork.name,
-  //       branch: 'master',
-  //     }),
-  //   };
-  //
-  //   return webhook(gh, link, undefined, gh).then(output => {
-  //     assert.deepEqual(output, {
-  //       status: 'ok',
-  //       pullRequest: {msg: `There's already a pull request on fork/my-app.`},
-  //       isEnabled: true,
-  //       many: false,
-  //       forkCount: 1,
-  //     });
-  //   });
-  // });
-  // it('should make a PR to a single fork of an upstream, but the link is disabled', () => {
-  //   const upstream = generateRepo({
-  //     owner: 'upstream', name: 'my-app',
-  //     default_branch: 'master',
-  //     branches: ['master'],
-  //   });
-  //   const fork = generateRepo({
-  //     owner: 'fork', name: 'my-app',
-  //     default_branch: 'master',
-  //     branches: ['master'],
-  //   });
-  //   const gh = createMockGithubInstance([upstream, fork]);
-  //
-  //   const upstreamId = generateId(),
-  //         forkId = generateId();
-  //   const link = {
-  //     enabled: false,
-  //     upstreamId,
-  //     upstream: () => Promise.resolve({
-  //       id: upstreamId,
-  //       type: 'repo',
-  //       owner: upstream.owner.login,
-  //       repo: upstream.name,
-  //       branch: 'master',
-  //     }),
-  //     forkId,
-  //     fork: () => Promise.resolve({
-  //       id: forkId,
-  //       type: 'repo',
-  //       owner: fork.owner.login,
-  //       repo: fork.name,
-  //       branch: 'master',
-  //     }),
-  //   };
-  //
-  //   return webhook(gh, link, undefined, gh).then(output => {
-  //     assert.deepEqual(output, {
-  //       error: 'not-enabled',
-  //       isEnabled: false,
-  //     });
-  //   });
-  // });
-  // it('should make a PR to a single fork of an upstream, but upstream / fork are null', () => {
-  //   const upstream = generateRepo({
-  //     owner: 'upstream', name: 'my-app',
-  //     default_branch: 'master',
-  //     branches: ['master'],
-  //   });
-  //   const fork = generateRepo({
-  //     owner: 'fork', name: 'my-app',
-  //     default_branch: 'master',
-  //     branches: ['master'],
-  //   });
-  //   const gh = createMockGithubInstance([upstream, fork]);
-  //
-  //   const link = {
-  //     enabled: true,
-  //     upstreamId: null,
-  //     upstream: () => Promise.resolve(null),
-  //     forkId: null,
-  //     fork: () => Promise.resolve(null),
-  //   };
-  //
-  //   return webhook(gh, link, undefined, gh).then(output => {
-  //     assert.deepEqual(output, {
-  //       error: 'upstream-or-fork-false',
-  //       isEnabled: true,
-  //       msg: 'Please set both a "upstream" and "fork" on this link.',
-  //     });
-  //   });
-  // });
-  // it('should not run a disabled webhook', () => {
-  //   const link = {
-  //     enabled: false,
-  //   };
-  //   const gh = createMockGithubInstance([]);
-  //
-  //   return webhook(gh, link, undefined, gh).then(output => {
-  //     assert.deepEqual(output, {
-  //       error: 'not-enabled',
-  //       isEnabled: false,
-  //     });
-  //   });
-  // });
+    const user = await User.create({username: 'ryan'});
+    const upstream = await Repository.create({type: 'repo', owner: 'foo', repo: 'bar', branch: 'master'});
+    const fork = await Repository.create({type: 'repo', owner: 'hello', repo: 'world', branch: 'master'});
+
+    const link = await Link.create({
+      name: 'My Link',
+      enabled: true,
+      hookId: ['123456'],
+      owner: user.id,
+      upstream: upstream.id,
+      fork: fork.id,
+    });
+
+    return webhook({
+      github: {
+        user: {
+          searchIssues,
+        },
+        bot: {
+          pullRequestsCreate,
+        },
+      }
+    }, link).catch(err => {
+      assert.equal(err.message, 'This repo opted out of backstroke pull requests');
+    });
+  });
+  it('should try to make a PR to a single fork of an upstream, but a pull request already exists', async () => {
+    const searchIssues = sinon.stub().resolves({total_count: 0, issues: []});
+    const pullRequestsCreate = sinon.stub().rejects({code: 422}); // PR already exists
+
+    const user = await User.create({username: 'ryan'});
+    const upstream = await Repository.create({type: 'repo', owner: 'foo', repo: 'bar', branch: 'master'});
+    const fork = await Repository.create({type: 'repo', owner: 'hello', repo: 'world', branch: 'master'});
+
+    const link = await Link.create({
+      name: 'My Link',
+      enabled: true,
+      hookId: ['123456'],
+      owner: user.id,
+      upstream: upstream.id,
+      fork: fork.id,
+    });
+
+    return webhook({
+      github: {
+        user: {
+          searchIssues,
+        },
+        bot: {
+          pullRequestsCreate,
+        },
+      }
+    }, link).catch(err => {
+      assert.equal(err.message, `There's already a pull request on hello/world.`);
+    });
+  });
+  it('should make a PR to a single fork of an upstream, but the link is disabled', async () => {
+    const link = await Link.create({
+      name: 'My Link',
+      enabled: false,
+      hookId: ['123456'],
+      owner: null,
+      upstream: null,
+      fork: null,
+    });
+
+    return webhook({}, link).then(resp => {
+      assert.deepEqual(resp, {error: 'not-enabled', isEnabled: false});
+    });
+  });
+  it('should make a PR to a single fork of an upstream, but upstream / fork are null', async () => {
+    const link = await Link.create({
+      name: 'My Link',
+      enabled: true,
+      hookId: ['123456'],
+      owner: null,
+      upstream: null,
+      fork: null,
+    });
+
+    return webhook({}, link).then(resp => {
+      assert.deepEqual(resp, {
+        error: 'upstream-or-fork-false',
+        isEnabled: true,
+        msg: 'Please set both a "upstream" and "fork" on this link.',
+      });
+    });
+  });
 });
